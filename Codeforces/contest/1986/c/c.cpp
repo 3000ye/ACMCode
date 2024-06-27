0927bcdee3a42640475c560a9fa857283d0d75d5
3000ye <hcg2924239214@163.com>
2024-06-27 15:25:01 +0800

    Codeforces race 1986 abc

--- Codeforces/contest/1986/c/c.cpp
+++ Codeforces/contest/1986/c/c.cpp
@@ -4,7 +4,27 @@ using namespace std;
 #define ld long double
 
 void solve() {
-    
+    int n, m; cin >> n >> m;
+    string s; cin >> s;
+    vector<int> ls; vector<bool> flag(n + 10);
+    for (int i = 0; i < m; i ++) {
+        int x; cin >> x;
+        if (flag[x]) continue;
+        
+        flag[x] = true;
+        ls.push_back(x);
+    }
+    string c; cin >> c;
+
+    sort(ls.begin(), ls.end());
+    sort(c.begin(), c.end());
+
+    for (int i = 0, l = ls.size(); i < l; i ++) {
+        int idx = ls[i] - 1;
+        s[idx] = c[i];
+    }
+
+    cout << s << endl;
 }
 
 int main() {
