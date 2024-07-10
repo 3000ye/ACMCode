12d9064ee02616acb7f84be0da5d3c72a88a4b7e
3000ye <hcg2924239214@163.com>
2024-07-10 21:57:41 +0800

    增加后处理 Wordflows:生成 Markdown 笔记

--- LeetCode/61.旋转链表.cpp
+++ LeetCode/61.旋转链表.cpp
@@ -27,15 +27,6 @@ struct ListNode {
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-// struct ListNode {
-//     int val;
-//     ListNode *next;
-//     ListNode() : val(0), next(nullptr) {}
-//     ListNode(int x) : val(x), next(nullptr) {}
-//     ListNode(int x, ListNode *next) : val(x), next(next) {}
-// };
-
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
