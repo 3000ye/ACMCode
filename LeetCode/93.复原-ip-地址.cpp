60b2db4ae5d7bad0e4f765869376d72a4c3481b8
3000ye <hcg2924239214@163.com>
2024-07-29 00:56:00 +0800

    2024力扣练习 36/100

--- LeetCode/93.复原-ip-地址.cpp
+++ LeetCode/93.复原-ip-地址.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <string>
 #include <vector>
 using namespace std;
 
@@ -11,12 +12,55 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    vector<string> restoreIpAddresses(string s) {
-        vector<string> res;
-        if (s.size() < 4) return res;
+    vector<string> res;
+    vector<int> path;
 
-        
+    vector<string> restoreIpAddresses(string s) {
+        if (s.size() < 4 or s.size() > 12) return res;
+        dfs(s, 0);
+        return res;
+    }
+
+    void dfs(string& s, int cur) {
+        // path 长度为 4 且 走到终点时终止
+        if (path.size() == 4) {
+            if (cur == s.size()) {
+                string temp = "";
+                for (auto i : path) { temp += std::to_string(i); temp += "."; }
+                temp.pop_back();
+
+                res.push_back(temp);
+            }
+
+            return;
+        }
+
+        // 走到终点但是 path 长度不够，直接终止
+        if (cur == s.size()) return;
+
+        // 开始搜索
+        // s[cur] = 0 的时候特判
+        if (s[cur] == '0') {
+            path.push_back(0);
+            dfs(s, cur + 1);
+
+            path.pop_back();
+            return;
+        }
+
+        // 一般情况
+        int sum = 0;
+        for (int i = cur, l = s.size(); i < l; i ++) {
+            sum = sum * 10 + (s[i] - '0');
+            if (sum <= 255) {
+                path.push_back(sum);
+                dfs(s, i + 1);
+
+                // 恢复现场
+                path.pop_back();
+            }
+            else break;
+        }
     }
 };
 // @lc code=end
-
--- test.cpp
+++ test.cpp
@@ -1,60 +1,18 @@
 #include "bits/stdc++.h"
 using namespace std;
+#define ll long long
+#define ld long double
 
-class Solution {
-public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<vector<int>> res;
-        priority_queue<tuple<int, int, int>> pque;
+void solve() {
+    string s = "123456";
+    auto temp = s.substr(2, 0);
 
-        for (int i = 0, l = nums1.size(); i < min(l, k); i ++) pque.emplace(-nums1[i] - nums2[0], i, 0);
-        while (res.size() < k and !pque.empty()) {
-            auto [_, i, j] = pque.top(); pque.pop();
-            res.push_back({nums1[i], nums2[j]});
-
-            if (j + 1 < nums2.size()) pque.emplace(-nums1[i] - nums2[j + 1], i, j + 1);
-        }
-        
-        return res;
-    }
-};
+    cout << temp << endl;
+}
 
 int main() {
-    int n,m,data,k;
-    vector<int> nums1,nums2;
-    cin>>n;
-    for(int i=0; i<n; i++)
-    {
-
-        cin>>data;
-
-        nums1.push_back(data);
-
-    }
-
-    cin>>m;
-    for(int i=0; i<m; i++)
-    {
-
-        cin>>data;
-
-        nums2.push_back(data);
-
-    }
-    cin>>k;
-    vector<vector<int> > res=Solution().kSmallestPairs(nums1,nums2,k);
-
-    vector<int> ans;
-    for (auto i : res) {
-        ans.push_back(i[0] + i[1]);
-    }
-
-    sort(ans.begin(), ans.end());
-    for (int i = 0; i < ans.size(); i ++) {
-        if (i > 0) cout << " ";
-        cout << ans[i];
-    }
-    cout << endl;
-
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    solve();
     return 0;
 }
