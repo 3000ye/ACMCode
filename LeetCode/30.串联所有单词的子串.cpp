7a335d930b46ab3ca62f96c702bf6bb010c17864
3000ye <hcg2924239214@163.com>
2024-08-15 14:36:10 +0800

    2024力扣练习 70/100

--- LeetCode/30.串联所有单词的子串.cpp
+++ LeetCode/30.串联所有单词的子串.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include <string>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
@@ -12,14 +14,31 @@ using namespace std;
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        int x = words.size(), y = words[0].size();
-        if (x * y > s.size()) return {};
-
-        set<string> target(words.begin(), words.end());
         vector<int> res;
-        for (int i = 0, l = x * y, n = s.size(); i + l < n; i ++) {
-            
+        int n = s.size(), x = words.size(), y = words[0].size(), l = x * y;
+        unordered_map<string, int> target;
+        for (auto w : words) target[w] ++;
+
+        vector<unordered_map<string, int>> ls(y);
+        for (int i = 0; i < y and i + l <= n; i ++) {
+            for (int j = i; j < i + l; j += y) {
+                auto w = s.substr(j, y);
+                ls[i][w] ++;
+            }
+
+            if (ls[i] == target) res.push_back(i);
         }
+
+        for (int i = y; i + l <= n; i ++) {
+            int r = i % y;
+            auto sa = s.substr(i - y, y), sb = s.substr(i + l - y, y);
+            if (-- ls[r][sa] == 0) ls[r].erase(sa);
+            ls[r][sb] ++;
+
+            if (ls[r] == target) res.push_back(i);
+        }
+
+        return res;
     }
 };
 // @lc code=end
